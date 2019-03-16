97373f4cb143f35e6c88e5b5754dae0412a0a0fc
Joseph Semrai <josephsemrai@gmail.com>
2019-03-16 16:19:48 -0400

    Inital Commit
    Before reworking of image upload process and CSS structure.

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# chat
+TigerChat
+===
+Chat application built with Node.js and socket.io
+Built upon https://github.com/wayou/HiChat with improvements to core functionality and design.
+
+Notable improvements/changes:
+---
+Added function to convert 24-hour time to 12-hour time
+Changed wording to better reflect functions and reduce confusion by the end user
+Added styling to buttons
+Added styling to login page
+Streamlined emoji support
+
+Forked from HiChat with the intention to bring modern design and more features to it.
+---
+ 
+A chat application built with Node.js and Socket.io.
+
+Setup Instructions
+---
+1. Dwnload the code then extract
+2. Run `npm install` from command line window
+3. Run `node server` or `iojs server` if you are using [io.js](https://iojs.org/)
+4. Open your browser and visit `localhost:3000`
---
+++ www/scripts/hichat.js
@@ -0,0 +1,188 @@
+/*
+ *hichat v0.4.2
+ *Wayou Mar 28,2014
+ *MIT license
+ *view on GitHub:https://github.com/wayou/HiChat
+ *see it in action:http://hichat.herokuapp.com/
+ */
+window.onload = function() {
+    var hichat = new HiChat();
+    hichat.init();
+};
+var HiChat = function() {
+    this.socket = null;
+};
+function tConvert (time) {
+    // Check correct time format and split into components
+    time = time.toString ().match (/^([01]\d|2[0-3])(:)([0-5]\d)(:[0-5]\d)?$/) || [time];
+  
+    if (time.length > 1) { // If time format correct
+      time = time.slice (1);  // Remove full string match value
+      time[5] = +time[0] < 12 ? 'AM' : 'PM'; // Set AM/PM
+      time[0] = +time[0] % 12 || 12; // Adjust hours
+    }
+    return time.join (''); // return adjusted time or original string
+  }
+  
+HiChat.prototype = {
+    init: function() {
+        var that = this;
+        this.socket = io.connect();
+        this.socket.on('connect', function() {
+            document.getElementById('info').textContent = 'Pick your username ✍🏻';
+            document.getElementById('nickWrapper').style.display = 'block';
+            document.getElementById('nicknameInput').focus();
+        });
+        this.socket.on('nickExisted', function() {
+            document.getElementById('info').textContent = 'Someone already took that username 😔, please pick another one.';
+        });
+        this.socket.on('loginSuccess', function() {
+            document.title = 'MCHS Chat | ' + document.getElementById('nicknameInput').value;
+            document.getElementById('loginWrapper').style.display = 'none';
+            document.getElementById('messageInput').focus();
+        });
+        this.socket.on('error', function(err) {
+            if (document.getElementById('loginWrapper').style.display == 'none') {
+                document.getElementById('status').textContent = 'Failed to connect :(';
+            } else {
+                document.getElementById('info').textContent = 'Failed to connect :(';
+            }
+        });
+        this.socket.on('system', function(nickName, userCount, type) {
+            var msg = nickName + (type == 'login' ? ' joined' : ' left');
+            that._displayNewMsg('System', msg, 'blue');
+            document.getElementById('status').textContent = userCount + (userCount > 1 ? ' users' : ' user') + ' online';
+        });
+        this.socket.on('newMsg', function(user, msg, color) {
+            that._displayNewMsg(user, msg, color);
+        });
+        this.socket.on('newImg', function(user, img, color) {
+            that._displayImage(user, img, color);
+        });
+        document.getElementById('loginBtn').addEventListener('click', function() {
+            var nickName = document.getElementById('nicknameInput').value;
+            if (nickName.trim().length != 0) {
+                that.socket.emit('login', nickName);
+            } else {
+                document.getElementById('nicknameInput').focus();
+            };
+        }, false);
+        document.getElementById('nicknameInput').addEventListener('keyup', function(e) {
+            if (e.keyCode == 13) {
+                var nickName = document.getElementById('nicknameInput').value;
+                if (nickName.trim().length != 0) {
+                    that.socket.emit('login', nickName);
+                };
+            };
+        }, false);
+        document.getElementById('sendBtn').addEventListener('click', function() {
+            var messageInput = document.getElementById('messageInput'),
+                msg = messageInput.value,
+                color = document.getElementById('colorStyle').value;
+            messageInput.value = '';
+            messageInput.focus();
+            if (msg.trim().length != 0) {
+                that.socket.emit('postMsg', msg, color);
+                that._displayNewMsg('You', msg, color);
+                return;
+            };
+        }, false);
+        document.getElementById('messageInput').addEventListener('keyup', function(e) {
+            var messageInput = document.getElementById('messageInput'),
+                msg = messageInput.value,
+                color = document.getElementById('colorStyle').value;
+            if (e.keyCode == 13 && msg.trim().length != 0) {
+                messageInput.value = '';
+                that.socket.emit('postMsg', msg, color);
+                that._displayNewMsg('You', msg, color);
+            };
+        }, false);
+        document.getElementById('clearBtn').addEventListener('click', function() {
+            document.getElementById('historyMsg').innerHTML = '';
+        }, false);
+        document.getElementById('sendImage').addEventListener('change', function() {
+            if (this.files.length != 0) {
+                var file = this.files[0],
+                    reader = new FileReader(),
+                    color = document.getElementById('colorStyle').value;
+                if (!reader) {
+                    that._displayNewMsg('system', '!your browser doesn\'t support fileReader', 'red');
+                    this.value = '';
+                    return;
+                };
+                reader.onload = function(e) {
+                    this.value = '';
+                    that.socket.emit('img', e.target.result, color);
+                    that._displayImage('me', e.target.result, color);
+                };
+                reader.readAsDataURL(file);
+            };
+        }, false);
+        this._initialEmoji();
+        document.getElementById('emoji').addEventListener('click', function(e) {
+            var emojiwrapper = document.getElementById('emojiWrapper');
+            emojiwrapper.style.display = 'block';
+            e.stopPropagation();
+        }, false);
+        document.body.addEventListener('click', function(e) {
+            var emojiwrapper = document.getElementById('emojiWrapper');
+            if (e.target != emojiwrapper) {
+                emojiwrapper.style.display = 'none';
+            };
+        });
+        document.getElementById('emojiWrapper').addEventListener('click', function(e) {
+            var target = e.target;
+            if (target.nodeName.toLowerCase() == 'img') {
+                var messageInput = document.getElementById('messageInput');
+                messageInput.focus();
+                messageInput.value = messageInput.value + '[emoji:' + target.title + ']';
+            };
+        }, false);
+    },
+    _initialEmoji: function() {
+        var emojiContainer = document.getElementById('emojiWrapper'),
+            docFragment = document.createDocumentFragment();
+        for (var i = 69; i > 0; i--) {
+            var emojiItem = document.createElement('img');
+            emojiItem.src = '../content/emoji/' + i + '.gif';
+            emojiItem.title = i;
+            docFragment.appendChild(emojiItem);
+        };
+        emojiContainer.appendChild(docFragment);
+    },
+    _displayNewMsg: function(user, msg, color) {
+        var container = document.getElementById('historyMsg'),
+            msgToDisplay = document.createElement('p'),
+            date = tConvert(new Date().toTimeString().substr(0, 8)), //change this if you want to get seconds with AM/PM back
+            //determine whether the msg contains emoji
+            msg = this._showEmoji(msg);
+        msgToDisplay.style.color = color || '#000';
+        msgToDisplay.innerHTML = '<span class="timespan">('+ date + ') </span>' + "<b>" + user + ": </b>" + msg;
+        container.appendChild(msgToDisplay);
+        container.scrollTop = container.scrollHeight;
+    },
+    _displayImage: function(user, imgData, color) {
+        var container = document.getElementById('historyMsg'),
+            msgToDisplay = document.createElement('p'),
+            date = new Date().toTimeString().substr(0, 8);
+        msgToDisplay.style.color = color || '#000';
+        msgToDisplay.innerHTML = user + '<span class="timespan">(' + date + '): </span> <br/>' + '<a href="' + imgData + '" target="_blank"><img src="' + imgData + '"/></a>';
+        container.appendChild(msgToDisplay);
+        container.scrollTop = container.scrollHeight;
+    },
+    _showEmoji: function(msg) {
+        var match, result = msg,
+            reg = /\[emoji:\d+\]/g,
+            emojiIndex,
+            totalEmojiNum = document.getElementById('emojiWrapper').children.length;
+        while (match = reg.exec(msg)) {
+            emojiIndex = match[0].slice(7, -1);
+            if (emojiIndex > totalEmojiNum) {
+                result = result.replace(match[0], '[X]');
+            } else {
+                result = result.replace(match[0], '<img class="emoji" src="../content/emoji/' + emojiIndex + '.gif" />');
+            };
+        };
+        return result;
+    }
+};
